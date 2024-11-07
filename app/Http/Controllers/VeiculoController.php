6147349dd44df4ced4384c073424bbef777642ce
Mateus Barreto <130594274+mateusbarret0@users.noreply.github.com>
2024-11-07 15:47:26 -0300

    fix: edit veiculo nao funciona e data do grid

--- app/Http/Controllers/VeiculoController.php
+++ app/Http/Controllers/VeiculoController.php
@@ -8,111 +8,181 @@ use Illuminate\Support\Facades\DB;
 class VeiculoController extends Controller
 {
     public function getVeiculos(Request $request)
-{
-    $searchTerm = $request->input('searchTerm');
+    {
+        $searchTerm = $request->input('searchTerm');
 
-    $veiculos = DB::table('VEICULOS as veic')
-        ->select(
-            'veic.modelo',
-            'veic.placa',
-            'veic.ano',
-            'veic.capacidade',
-            'veic.dt_prox_manu',
-            'veic.dt_ultim_manu',
-            'veic.empresa',
-            'veic.motorista',
-            'veic.tipo_veiculo'
-        )
-        ->get();
+        $veiculos = DB::table('VEICULOS as veic')
+            ->select(
+                'veic.id',
+                'veic.modelo',
+                'veic.placa',
+                'veic.ano',
+                'veic.capacidade',
+                'veic.dt_prox_manu',
+                'veic.dt_ultim_manu',
+                'veic.empresa',
+                'veic.motorista',
+                'veic.tipo_veiculo',
+                'veic.status'
+            );
 
-    return response($veiculos, 200);  
-}
+        return response($veiculos, 200);
+    }
 
-    
+
     public function insertVeiculos(Request $request)
-{
-    $veiculo = DB::table('VEICULOS')->insert([
-        'modelo' => $request->input('modelo'),
-        'placa' => $request->input('placa'),
-        'ano' => $request->input('ano'),
-        'capacidade' => $request->input('capacidade'),
-        'dt_prox_manu' => $request->input('dataProxManutencao'),
-        'dt_ultim_manu' => $request->input('dataUltManutencao'),
-        'empresa' => $request->input('empresa'),
-        'motorista' => $request->input('motorista'),
-        'tipo_veiculo' => $request->input('tipoVeiculo'),
-    ]);
-
-    return response()->json(['success' => true, 'message' => 'Veículo cadastrado com sucesso!'], 200);
-}
-public function deleteVeiculos(Request $request)
-{
-    $placa = $request->input('placa'); 
-
-    if (!$placa) {
-        return response()->json(['success' => false, 'message' => 'Placa do veículo não fornecido'], 400);
-    }
-
-  
-    $deleted = DB::table('VEICULOS')->where('placa', $placa)->delete();
-
-    if ($deleted) {
-        return response()->json(['success' => true, 'message' => 'Veículo excluído com sucesso!'], 200);
-    } else {
-        return response()->json(['success' => false, 'message' => 'Erro ao excluir veículo ou veículo não encontrado'], 400);
-    }
-}
-public function editVeiculos(Request $request)
-{
-    $veiculo = DB::table('VEICULOS')->where('placa', $request->placa)->first();
-
-    if (!$veiculo) {
-        return response()->json([
-            'status' => 'error',
-            'message' => 'Veículo não encontrado',
-        ], 404);
-    }
-
-    DB::table('VEICULOS')
-        ->where('placa', $request->placa)
-        ->update([
-            'modelo' => $request->modelo,
-            'ano' => $request->ano,
-            'capacidade' => $request->capacidade,
-            'dt_prox_manu' => $request->dataProxManutencao,
-            'dt_ultim_manu' => $request->dataUltManutencao,
-            'empresa' => $request->empresa,
-            'motorista' => $request->motorista,
-            'tipo_veiculo' => $request->tipoVeiculo,
+    {
+        $veiculo = DB::table('VEICULOS')->insert([
+            'modelo' => $request->input('modelo'),
+            'placa' => $request->input('placa'),
+            'ano' => $request->input('ano'),
+            'capacidade' => $request->input('capacidade'),
+            'dt_prox_manu' => $request->input('dataProxManutencao'),
+            'dt_ultim_manu' => $request->input('dataUltManutencao'),
+            'empresa' => $request->input('empresa'),
+            'motorista' => $request->input('motorista'),
+            'tipo_veiculo' => $request->input('tipoVeiculo'),
         ]);
 
-    return response()->json([
-        'status' => 'success',
-        'message' => 'Veículo atualizado com sucesso',
-    ], 200);
-}
+        return response()->json(['success' => true, 'message' => 'Veículo cadastrado com sucesso!'], 200);
+    }
+    public function deleteVeiculos(Request $request)
+    {
+        $placa = $request->input('placa');
+
+        if (!$placa) {
+            return response()->json(['success' => false, 'message' => 'Placa do veículo não fornecido'], 400);
+        }
+
+
+        $deleted = DB::table('VEICULOS')->where('placa', $placa)->delete();
+
+        if ($deleted) {
+            return response()->json(['success' => true, 'message' => 'Veículo excluído com sucesso!'], 200);
+        } else {
+            return response()->json(['success' => false, 'message' => 'Erro ao excluir veículo ou veículo não encontrado'], 400);
+        }
+    }
+    public function editVeiculos(Request $request)
+    {
+        $veiculo = DB::table('VEICULOS')->where('placa', $request->placa)->first();
+
+        if (!$veiculo) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Veículo não encontrado',
+            ], 404);
+        }
+
+        DB::table('VEICULOS')
+            ->where('placa', $request->placa)
+            ->update([
+                'modelo' => $request->modelo,
+                'ano' => $request->ano,
+                'capacidade' => $request->capacidade,
+                'dt_prox_manu' => $request->dataProxManutencao,
+                'dt_ultim_manu' => $request->dataUltManutencao,
+                'empresa' => $request->empresa,
+                'motorista' => $request->motorista,
+                'tipo_veiculo' => $request->tipoVeiculo,
+            ]);
+
+        return response()->json(['success' => true, 'message' => 'Veículo cadastrado com sucesso!'], 200);
+    }
+    public function deleteVeiculos(Request $request)
+    {
+        $placa = $request->input('placa');
+
+        if (!$placa) {
+            return response()->json(['success' => false, 'message' => 'Placa do veículo não fornecido'], 400);
+        }
+
+
+        $deleted = DB::table('VEICULOS')->where('placa', $placa)->delete();
+
+        if ($deleted) {
+            return response()->json(['success' => true, 'message' => 'Veículo excluído com sucesso!'], 200);
+        } else {
+            return response()->json(['success' => false, 'message' => 'Erro ao excluir veículo ou veículo não encontrado'], 400);
+        }
+    }
+    public function editVeiculos(Request $request)
+    {
+        // dd($request->all());
+        $veiculo = DB::table('VEICULOS')->where('id', $request->numVeic)->first();
+
+        if (!$veiculo) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Veículo não encontrado',
+            ], 404);
+        }
+
+        DB::table('VEICULOS')
+            ->where('id', $request->numVeic)
+            ->update([
+                'modelo' => $request->modelo,
+                'ano' => $request->ano,
+                'capacidade' => $request->capacidade,
+                'placa' => $request->placa,
+                'dt_prox_manu' => $request->dataProxManutencao,
+                'dt_ultim_manu' => $request->dataUltManutencao,
+                'empresa' => $request->empresa,
+                'motorista' => $request->motorista,
+                'tipo_veiculo' => $request->tipoVeiculo,
+            ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Veículo atualizado com sucesso',
+        ], 200);
+    }
+
+    public function editStatusVeiculo(Request $request)
+    {
+        // dd($request->all());
+        $veiculo = DB::table('VEICULOS')->where('placa', $request->placa)->first();
+
+        if (!$veiculo) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Veículo não encontrado',
+            ], 404);
+        }
+
+        DB::table('VEICULOS')
+            ->where('placa', $request->placa)
+            ->update([
+                'status' => $request->status,
+            ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Status do veículo atualizado com sucesso',
+        ], 200);
+    }
 
 
 
-public function insertRotas(Request $request)
-{
-    $veiculo = DB::table('ROTAS')->insert([
-        'LOCAL_PARTIDA' => $request->input('localPartida'),
-        'LOCAL_CHEGADA' => $request->input('localChegada'),
-    ]);
+    // public function insertRotas(Request $request)
+    // {
+    //     $veiculo = DB::table('ROTAS')->insert([
+    //         'LOCAL_PARTIDA' => $request->input('localPartida'),
+    //         'LOCAL_CHEGADA' => $request->input('localChegada'),
+    //     ]);
 
-    return response()->json(['success' => true, 'message' => 'Veículo cadastrado com sucesso!'], 200);
-}
-public function getRotas(Request $request)
-{
+    //     return response()->json(['success' => true, 'message' => 'Veículo cadastrado com sucesso!'], 200);
+    // }
+    // public function getRotas(Request $request)
+    // {
 
-    $rotas = DB::table('ROTAS as r')
-        ->select(
-            'r.LOCAL_PARTIDA',
-            'r.LOCAL_CHEGADA',
-        )
-        ->get();
+    //     $rotas = DB::table('ROTAS as r')
+    //         ->select(
+    //             'r.LOCAL_PARTIDA',
+    //             'r.LOCAL_CHEGADA',
+    //         )
+    //         ->get();
 
-    return response($rotas, 200);  
-}
+    //     return response($rotas, 200);
+    // }
 }
