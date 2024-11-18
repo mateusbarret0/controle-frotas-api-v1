4ac6565d1faa13463e2ce25822f18810781c8747
Mateus Barreto <130594274+mateusbarret0@users.noreply.github.com>
2024-11-18 17:54:06 -0300

    fix modal rotas

--- app/Http/Controllers/RotasController.php
+++ app/Http/Controllers/RotasController.php
@@ -20,73 +20,75 @@ class RotasController extends Controller
     }
 
     public function insertRotas(Request $request)
-{
-    $rota = DB::table('ROTAS')->insert([
-        'CEP_PARTIDA' => $request->input('cepPartida'),
-        'CEP_CHEGADA' => $request->input('cepChegada'),
-        'NUMERO_PARTIDA' => $request->input('numeroPartida'),
-        'NUMERO_CHEGADA' => $request->input('numeroChegada'),
-        'DESCRICAO_PARTIDA' => $request->input('descricaoPartida'),
-        'DESCRICAO_CHEGADA' => $request->input('descricaoChegada'),
-        'COMPLEMENTO_PARTIDA' => $request->input('complementoPartida'),
-        'COMPLEMENTO_CHEGADA' => $request->input('complementoChegada'),
-        'RUA_PARTIDA' => $request->input('enderecoPartida.rua'),
-        'BAIRRO_PARTIDA' => $request->input('enderecoPartida.bairro'),
-        'CIDADE_PARTIDA' => $request->input('enderecoPartida.cidade'),
-        'ESTADO_PARTIDA' => $request->input('enderecoPartida.estado'),
-        'RUA_CHEGADA' => $request->input('enderecoChegada.rua'),
-        'BAIRRO_CHEGADA' => $request->input('enderecoChegada.bairro'),
-        'CIDADE_CHEGADA' => $request->input('enderecoChegada.cidade'),
-        'ESTADO_CHEGADA' => $request->input('enderecoChegada.estado'),
-        'DATA_HORA_INICIO' => now(), 
-        'DATA_HORA_CHEGADA' => now()->addHours(6),  
-    ]);
+    {
+        $placa = $request->input('veiculo.placa');
+        $rota = DB::table('ROTAS')->insert([
+            'CEP_PARTIDA' => $request->input('cepPartida'),
+            'CEP_CHEGADA' => $request->input('cepChegada'),
+            'NUMERO_PARTIDA' => $request->input('numeroPartida'),
+            'NUMERO_CHEGADA' => $request->input('numeroChegada'),
+            'DESCRICAO_PARTIDA' => $request->input('descricaoPartida'),
+            'DESCRICAO_CHEGADA' => $request->input('descricaoChegada'),
+            'COMPLEMENTO_PARTIDA' => $request->input('complementoPartida'),
+            'COMPLEMENTO_CHEGADA' => $request->input('complementoChegada'),
+            'RUA_PARTIDA' => $request->input('enderecoPartida.rua'),
+            'BAIRRO_PARTIDA' => $request->input('enderecoPartida.bairro'),
+            'CIDADE_PARTIDA' => $request->input('enderecoPartida.cidade'),
+            'ESTADO_PARTIDA' => $request->input('enderecoPartida.estado'),
+            'RUA_CHEGADA' => $request->input('enderecoChegada.rua'),
+            'BAIRRO_CHEGADA' => $request->input('enderecoChegada.bairro'),
+            'CIDADE_CHEGADA' => $request->input('enderecoChegada.cidade'),
+            'ESTADO_CHEGADA' => $request->input('enderecoChegada.estado'),
+            'DATA_HORA_INICIO' => now(),
+            'DATA_HORA_CHEGADA' => now()->addHours(2),
+            'PLACA' => $placa,
+        ]);
 
-    return response()->json(['success' => true, 'message' => 'Rota cadastrada com sucesso!'], 200);
-}
+        return response()->json(['success' => true, 'message' => 'Rota cadastrada com sucesso!'], 200);
+    }
 
 
-public function getRotas(Request $request)
-{
-    $searchTerm = $request->input('searchTerm');
+    public function getRotas(Request $request)
+    {
+        $placa = $request->query('placa');
+        $rotas = DB::table('rotas as r')
+            ->join('veiculos as v', 'v.placa', '=', 'r.placa')
+            ->select(
+                'r.ID',
+                'r.CEP_PARTIDA',
+                'r.CEP_CHEGADA',
+                'r.NUMERO_PARTIDA',
+                'r.NUMERO_CHEGADA',
+                'r.DESCRICAO_PARTIDA',
+                'r.DESCRICAO_CHEGADA',
+                'r.COMPLEMENTO_PARTIDA',
+                'r.COMPLEMENTO_CHEGADA',
+                'r.RUA_PARTIDA',
+                'r.BAIRRO_PARTIDA',
+                'r.CIDADE_PARTIDA',
+                'r.ESTADO_PARTIDA',
+                'r.RUA_CHEGADA',
+                'r.BAIRRO_CHEGADA',
+                'r.CIDADE_CHEGADA',
+                'r.ESTADO_CHEGADA',
+                'r.DATA_HORA_INICIO',
+                'r.DATA_HORA_CHEGADA'
+            )
+            ->where('v.placa', $placa)
+            ->get();
 
-    $rotas = DB::table('rotas as r')
-    ->select(
-        'r.ID',
-        'r.CEP_PARTIDA',
-        'r.CEP_CHEGADA',
-        'r.NUMERO_PARTIDA',
-        'r.NUMERO_CHEGADA',
-        'r.DESCRICAO_PARTIDA',
-        'r.DESCRICAO_CHEGADA',
-        'r.COMPLEMENTO_PARTIDA',
-        'r.COMPLEMENTO_CHEGADA',
-        'r.RUA_PARTIDA',
-        'r.BAIRRO_PARTIDA',
-        'r.CIDADE_PARTIDA',
-        'r.ESTADO_PARTIDA',
-        'r.RUA_CHEGADA',
-        'r.BAIRRO_CHEGADA',
-        'r.CIDADE_CHEGADA',
-        'r.ESTADO_CHEGADA',
-        'r.DATA_HORA_INICIO',
-        'r.DATA_HORA_CHEGADA'
-    )
-    ->get();
+        // if ($request->has('search') && $request->input('search') != '') {
+        //     $searchTerm = $request->input('search');
 
-    // if ($request->has('search') && $request->input('search') != '') {
-    //     $searchTerm = $request->input('search');
+        //     $rotas->where(function ($subQuery) use ($searchTerm) {
+        //         $subQuery->where('veic.modelo', 'LIKE', '%' . $searchTerm . '%')
+        //             ->orWhere('veic.placa', 'LIKE', '%' . $searchTerm . '%')
+        //             ->orWhere('veic.motorista', 'LIKE', '%' . $searchTerm . '%');
+        //     });
+        // }
 
-    //     $rotas->where(function ($subQuery) use ($searchTerm) {
-    //         $subQuery->where('veic.modelo', 'LIKE', '%' . $searchTerm . '%')
-    //             ->orWhere('veic.placa', 'LIKE', '%' . $searchTerm . '%')
-    //             ->orWhere('veic.motorista', 'LIKE', '%' . $searchTerm . '%');
-    //     });
-    // }
+        // $rotas = $rotas->get();
 
-    // $rotas = $rotas->get();
-
-    return response($rotas, 200);
-}
-
+        return response($rotas, 200);
+    }
 }
